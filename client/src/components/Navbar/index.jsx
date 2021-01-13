c0a0473e3aaf411751d0b60becf23b51d0d98afd
yachen <io260188@gmail.com>
2021-01-13 17:48:06 +0800

    feat: navbar

---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.scss';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h3 className="title">Chatroom</h3>
+    </nav>
+  );
+};
+
+export default Navbar;
