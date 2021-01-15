7435a8b2c791a966f0af2f569354a825dba40128
yachen <io260188@gmail.com>
2021-01-16 00:04:15 +0800

    fix: ModOption:hover

--- client/src/components/ModeOption/index.jsx
+++ client/src/components/ModeOption/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './index.scss';
 
-const ModeOption = ({ option, isActive, onClick }) => {
+const ModeOption = ({ option, onClick }) => {
   return (
-    <h2 className={`mode_option ${isActive ? 'active' : ''}`} onClick={onClick}>
+    <h2 className="mode_option" onClick={onClick}>
       {option}
     </h2>
   );
