d00266ccdea8f2e447bf00fa72ea5a9957ca232d
yachen <io260188@gmail.com>
2021-01-19 19:33:23 +0800

    test

--- src/server/index.js
+++ src/server/index.js
@@ -61,8 +61,6 @@ io.on('connect', (socket) => {
     const userList = removeUser(userInfo, roomInfo);
     const sumOfUsersInRooms = getSumOfUsersInExistRooms();
 
-    objRoomInfo = [{...objRoomInfo, room:''}];
-
     socket.leave(roomInfo.room);
 
     io.to(roomInfo.room).emit('receiveUserList', { userList });
