6d307fd1417f9efdf6ebb45cc79ca4921a6980b0
yachen <io260188@gmail.com>
2021-01-19 18:43:27 +0800

    delete: docs

--- src/redux/actions/index.js
+++
@@ -1,9 +0,0 @@
-import * as types from './constant';
-
-export const joinRoom = data => ({type: types.JOIN_ROOM, data});
-export const exitRoom = data => ({type: types.EXIT_ROOM, data});
-
-export const sendMessage = data => ({type: types.SEND_MESSAGE, data});
-export const receiveMessage = data => ({type: types.RECEIVE_MESSAGE, data});
-export const sendImage = data => ({type: types.SEND_IMAGE, data});
-export const receiveImage = data => ({type: types.RECEIVE_IMAGE, data});
